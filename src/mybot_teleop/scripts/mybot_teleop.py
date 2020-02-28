a947da2751555f2bd4d321fa24cd638d6a8550aa
DieJacquesDie <jacques.die2@outlook.com>
2020-02-28 17:49:07 +0100

    week3

--- src/mybot_teleop/scripts/mybot_teleop.py
+++ src/mybot_teleop/scripts/mybot_teleop.py
@@ -3,14 +3,20 @@
 
 import rospy
 import sys, termios, tty
-
 from geometry_msgs.msg import Twist
 
 
-linear_value=1
-angular_value=1
+# Function to Publish
+# =============================================================
+def simple_publisher(msg):
 
-
+    pub=rospy.Publisher('/turtle1/cmd_vel',Twist,queue_size=10)
+    rospy.init_node("simple_publisher", anonymous=True)
+    rate = rospy.Rate(10)
+    if not rospy.is_shutdown():
+	rospy.loginfo(msg)
+	pub.publish(msg)
+	rate.sleep()
 
 
 # Function to get which key is pressed in the terminal
@@ -25,47 +31,81 @@ def get_ch():
         termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
     return ch
 
-def simple_publisher(t):
-    pub=rospy.Publisher('/turtle1/cmd_vel',Twist,queue_size=10) #publish ds cmd_vel de type float64 avec q10
-    rospy.init_node('mybot_teleop', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    if not rospy.is_shutdown():
-        rospy.loginfo(t)
-        pub.publish(t)
-        rate.sleep()
-while __name__ == '__main__':
 
-    try:
-        # How to use the get_ch function
-        char = get_ch()
-        commande=Twist()
-        commande.linear.x = linear_value
-        commande.linear.y = 0
-        commande.linear.z = 0
-        commande.angular.x = 0
-        commande.angular.y = 0
-        commande.angular.z = 0
-        if char == "\x1b[A":  # UP key
+if __name__ == '__main__':
+
+    cmd = Twist()
+    cmd.linear.x = 0
+    cmd.linear.y = 0
+    cmd.linear.z = 0
+    cmd.angular.x = 0
+    cmd.angular.y = 0
+    cmd.angular.z = 0
+
+    linear_vel = rospy.get_param("linear_scale")
+    angular_vel = 1 #rospy.get_param("/angular_scale")
+
+    ##linear_init = rospy.get_param("/linear_init")
+    ##angular_init = rospy.get_param("/angular_init")
+
+    while not rospy.is_shutdown():
+        try:
+            # How to use the get_ch function
+            char = get_ch()
+
+
+            if char == "\x1b[A":  # UP key
+	        cmd.angular.z = 0
+                cmd.linear.x += linear_vel
+		if cmd.linear.x > 1:
+		    cmd.linear.x = 1
+		    print("max linear vel reached")
+		print(cmd.linear.x)
+	        simple_publisher(cmd)
+            if char == "\x1b[B":  # DOWN key
+	        cmd.angular.z = 0
+                cmd.linear.x += -linear_vel
+		if cmd.linear.x < -1:
+		    cmd.linear.x = -1
+		    print("max linear vel reached")
+		print(cmd.linear.x)
+	        simple_publisher(cmd)
+            if char == "\x1b[C":  # RIGHT key
+                cmd.linear.x = 0
+	        cmd.angular.z += -angular_vel
+		if cmd.angular.z < -1:
+		    cmd.angular.z = -1
+		    print("max angular vel reached")
+		print(cmd.angular.x)
+	        simple_publisher(cmd)
+            if char == "\x1b[D":  # LEFT
+                cmd.linear.x = 0
+	        cmd.angular.z += angular_vel
+		if cmd.angular.z > 1:
+		    cmd.angular.z = 1
+		    print("max angular vel reached")
+		print(cmd.angular.x)
+	        simple_publisher(cmd)
+            if char == "qqq":  # QUIT
+		cmd.linear.x = 0
+   	        cmd.linear.y = 0
+    	        cmd.linear.z = 0
+                cmd.angular.x = 0
+                cmd.angular.y = 0
+                cmd.angular.z = 0
+		simple_publisher(cmd)
+                break
+            if char == "sss":  # STOP
+		cmd.linear.x = 0
+   	        cmd.linear.y = 0
+    	        cmd.linear.z = 0
+                cmd.angular.x = 0
+                cmd.angular.y = 0
+                cmd.angular.z = 0
+		simple_publisher(cmd)
+		print("movement stopped")
+
+
+
+        except rospy.ROSInterruptException:
             pass
-        if char == "\x1b[B":  # DOWN key
-            commande.linear.x = -linear_value
-
-        if char == "\x1b[C":  # RIGHT key
-            commande.angular.z = -angular_value
-
-        if char == "\x1b[D":  # LEFT
-            commande.angular.z = angular_value
-
-        if char == "qqq":  # QUIT
-            rospy.is_shutdown()
-        if char == "sss":  # STOP
-            commande.linear.x = 0
-            commande.linear.y = 0
-            commande.linear.z = 0
-            commande.angular.x = 0
-            commande.angular.y = 0
-            commande.angular.z = 0
-
-        simple_publisher(commande)
-    except rospy.ROSInterruptException:
-        pass
